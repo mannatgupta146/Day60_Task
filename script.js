7096e5889796613c62ba3f146752eaf8f3e837ae
mannatgupta146 <mannatgupta146@gmail.com>
2025-12-11 14:59:15 +0530

    2 also done

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-// # Day 60 — Exercises
+// // # Day 60 — Exercises
 
-// ---
+// // ---
 
-// ## Exercise 1 — Very Easy (Warming up)
+// // ## Exercise 1 — Very Easy (Warming up)
 
-// **Task (Hindi):** Ek function banao `afterDelay`
+// // **Task (Hindi):** Ek function banao `afterDelay`
 
-// **Requirements:**
-// - Ye function do cheezein lega:
-//   1. `time` (milliseconds)
-//   2. `callback` function
-// - Given `time` ke baad `callback` call kare
-// - Callback ke andar `"Callback executed"` print hona chahiye
+// // **Requirements:**
+// // - Ye function do cheezein lega:
+// //   1. `time` (milliseconds)
+// //   2. `callback` function
+// // - Given `time` ke baad `callback` call kare
+// // - Callback ke andar `"Callback executed"` print hona chahiye
 
-function afterDelay(time,cb){
-    setTimeout(() => {
-        cb()
-    }, time);
-}
+// function afterDelay(time,cb){
+//     setTimeout(() => {
+//         cb()
+//     }, time);
+// }
 
-afterDelay(1000, function(){
-    console.log("callback executed")
-})
+// afterDelay(1000, function(){
+//     console.log("callback executed")
+// })
 
-// **Use case:**
-// > “2 second baad ek kaam karna hai”
+// // **Use case:**
+// // > “2 second baad ek kaam karna hai”
 
-afterDelay(2000, function(){
-    console.log("2 second baad ek kaam karna hai")
-})
+// afterDelay(2000, function(){
+//     console.log("2 second baad ek kaam karna hai")
+// })
 
-// **Goal:**
-// - Samajhna ki callback delay ke baad kaise execute hota hai
-// - Ye `setTimeout` + callback connection hai
+// // **Goal:**
+// // - Samajhna ki callback delay ke baad kaise execute hota hai
+// // - Ye `setTimeout` + callback connection hai
 
-// ---
+// // ---
 
 // ## Exercise 2 — Intermediate (Data flow)
 
@@ -46,6 +46,12 @@ afterDelay(2000, function(){
 //   - `id`
 //   - `username`
 
+function getUser(username, cb1){
+    setTimeout(() => {
+        cb1({username, age: 22, userId: 15281})
+    }, 1000);
+}
+
 // **Then:**
 // - Callback ke andar ek aur function call karo `getUserPosts`
 
@@ -53,9 +59,22 @@ afterDelay(2000, function(){
 // - `userId` lega
 // - 1 second ke baad `callback` ko `posts` ka array de
 
+function getUserPosts(userId, cb2){
+    console.log("getting user posts...")
+    setTimeout(() => {
+        cb2(["post1", "post2", "post3"])
+    }, 1000);
+}
+
 // **Final output:**
 // - User ka `username` print ho
 // - Fir uske `posts` print ho
+
+getUser("Mannat", function(data){
+    getUserPosts(data.userId, function(posts){
+        console.log(data.username, data, posts)
+    })
+})
 
 // **Goal:**
 // - Samajhna ki ek async ka result next async ko kaise milta hai
